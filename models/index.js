4a4c4a2aa5d8c5188f59f4de1612a07c973b1124
ahrum noh <sydneyroseenglish@gmail.com>
2021-09-17 13:53:20 +1000

    push

--- models/index.js
+++ models/index.js
@@ -2,12 +2,13 @@ const Post = require('./Post');
 const User = require('./User');
 const Comment = require('./Comment');
 
-User.hasMany(Post, {
-  foreignKey: 'user_id'
-});
 
 Post.belongsTo(User, {
   foreignKey: 'user_id',
+});
+
+User.hasMany(Post, {
+  foreignKey: 'user_id'
 });
 
 Post.hasMany(Comment, {
--- server.js
+++ server.js
@@ -12,10 +12,6 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 const exphbs = require('express-handlebars');
-const hbs = exphbs.create({ helpers });
-
-app.engine('handlebars', hbs.engine);
-app.set('view engine', 'handlebars');
 
 const session = require('express-session');
 
@@ -35,6 +31,11 @@ const sess = {
 };
 
 app.use(session(sess));
+
+const hbs = exphbs.create({ helpers });
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
 
 app.use(routes);
 
